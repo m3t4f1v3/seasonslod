003e1a0c821fa79b823136b4a086a72ce9d6fd1b
metafive <40429600+m3t4f1v3@users.noreply.github.com>
2025-12-08 20:53:13 +1100

    refactor a bit too

--- src/main/java/io/github/m3t4f1v3/ListenerStuff.java
+++ src/main/java/io/github/m3t4f1v3/ListenerStuff.java
@@ -6,56 +6,69 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerRegisterChannelEvent;
 
+import com.destroystokyo.paper.event.server.ServerTickStartEvent;
+
 import me.casperge.realisticseasons.RealisticSeasons;
-import me.casperge.realisticseasons.api.SeasonChangeEvent;
 import me.casperge.realisticseasons.api.SeasonsAPI;
-import me.casperge.realisticseasons.calendar.DayChangeEvent;
+import me.casperge.realisticseasons.calendar.Date;
 import me.casperge.realisticseasons.season.Season;
 import me.casperge.realisticseasons.season.SubSeason;
 import xyz.bluspring.modernnetworking.bukkit.api.BukkitNetworkSender;
 
 public class ListenerStuff implements Listener {
 
-    private static SubSeason currentSubSeason;
+    private Season currentSeason = null;
+    private int currentSubSeasonPhase = -1;
+
+
+    private void sendData(Player player) {
+        String seasonName = currentSeason.name();
+        BukkitNetworkSender.sendPacketServer(player, new Networking.SeasonPacket(SeasonalLods.biomeJson, seasonName, currentSubSeasonPhase, RealisticSeasons.getInstance().getSettings().subSeasonsEnabled));
+        // BukkitNetworkSender.sendPacketServer(player, new Networking.ReloadPacket()); 
+    }
+
+    private void updateDataThenSend(Player player, boolean override) {
+        Season season = SeasonsAPI.getInstance().getSeason(player.getWorld());
+        int subSeasonPhase = getCurrentSubSeason(SeasonsAPI.getInstance().getDate(player.getWorld()), player.getWorld().getFullTime()).getPhase();
+        if (season != currentSeason || subSeasonPhase != currentSubSeasonPhase || override) {
+            currentSeason = season;
+            currentSubSeasonPhase = subSeasonPhase;
+            sendData(player);
+        }
+    }
 
     @EventHandler
     public void onPlayerJoin(PlayerRegisterChannelEvent event) {
         Player player = event.getPlayer();
-        World world = player.getWorld();
-        Season season = SeasonsAPI.getInstance().getSeason(world);
-        BukkitNetworkSender.sendPacketServer(player, new Networking.BiomePacket(SeasonalLods.biomeJson));
-        BukkitNetworkSender.sendPacketServer(player, new Networking.SeasonPacket(season.name()));
-        if (RealisticSeasons.getInstance().getSettings().subSeasonsEnabled) {
-            currentSubSeason = RealisticSeasons.getInstance().getTimeManager().getCorrectSubSeason(world);
-            BukkitNetworkSender.sendPacketServer(player, new Networking.SubSeasonPacket(currentSubSeason.getPhase()));
-        }
+        updateDataThenSend(player, true);
         // BukkitNetworkSender.sendPacketServer(player, new Networking.ReloadPacket());
     }
 
-    @EventHandler
-    public void onSeasonChange(SeasonChangeEvent e) {
-        Season season = e.getNewSeason();
-        World world = e.getWorld();
-        for (Player player : world.getPlayers()) {
-            BukkitNetworkSender.sendPacketServer(player, new Networking.SeasonPacket(season.name()));
-            if (RealisticSeasons.getInstance().getSettings().subSeasonsEnabled) {
-                currentSubSeason = RealisticSeasons.getInstance().getTimeManager().getCorrectSubSeason(world);
-                BukkitNetworkSender.sendPacketServer(player, new Networking.SubSeasonPacket(currentSubSeason.getPhase()));
+    SubSeason getCurrentSubSeason(Date date, long fullTime) {
+        if (date == null) {
+            return SubSeason.MIDDLE;
+        } else {
+            Long time = fullTime % 24000L;
+            double var4 = RealisticSeasons.getInstance().getTimeManager().getSeasonProgressPercentage(date, time);
+            if (var4 < 9.0D) {
+                return SubSeason.START;
+            } else if (var4 >= 9.0D && var4 < 18.0D) {
+                return SubSeason.EARLY;
+            } else if (var4 >= 18.0D && var4 < 84.0D) {
+                return SubSeason.MIDDLE;
+            } else if (var4 >= 84.0D && var4 < 92.0D) {
+                return SubSeason.LATE;
+            } else {
+                return var4 >= 92.0D ? SubSeason.END : SubSeason.MIDDLE;
             }
-            BukkitNetworkSender.sendPacketServer(player, new Networking.ReloadPacket());
         }
     }
 
     @EventHandler
-    public void dayChange(DayChangeEvent e) {
-        if (RealisticSeasons.getInstance().getSettings().subSeasonsEnabled) {
-            int phase = RealisticSeasons.getInstance().getTimeManager().getCorrectSubSeason(e.getWorld()).getPhase();
-            if (currentSubSeason == null || currentSubSeason.getPhase() != phase) {
-                currentSubSeason = RealisticSeasons.getInstance().getTimeManager().getCorrectSubSeason(e.getWorld());
-                for (Player player : e.getWorld().getPlayers()) {
-                    BukkitNetworkSender.sendPacketServer(player, new Networking.SubSeasonPacket(currentSubSeason.getPhase()));
-                    BukkitNetworkSender.sendPacketServer(player, new Networking.ReloadPacket());
-                }
+    public void onTick(ServerTickStartEvent event) {
+        for (World world : RealisticSeasons.getInstance().getServer().getWorlds()) {
+            for (Player player : world.getPlayers()) {
+                updateDataThenSend(player, false);
             }
         }
     }
--- src/main/java/io/github/m3t4f1v3/Networking.java
+++ src/main/java/io/github/m3t4f1v3/Networking.java
@@ -17,51 +17,17 @@ public class Networking {
     
     public static final PacketDefinition<SeasonPacket, ByteBuf> SEASON_PACKET = NETWORK_REGISTRY.registerClientbound("sync_season", SeasonPacket.CODEC);
 
-    public record SeasonPacket(String season) implements NetworkPacket {
+    public record SeasonPacket(String json, String season, int subSeason, boolean useSubSeasons) implements NetworkPacket {
         public static final NetworkCodec<SeasonPacket, ByteBuf> CODEC = CompositeCodecs.composite(
+                NetworkCodecs.STRING_UTF8, SeasonPacket::json,
                 NetworkCodecs.STRING_UTF8, SeasonPacket::season,
+                NetworkCodecs.INT, SeasonPacket::subSeason,
+                NetworkCodecs.BOOL, SeasonPacket::useSubSeasons,
                 SeasonPacket::new);
 
         @Override
         public PacketDefinition<? extends NetworkPacket, ? extends ByteBuf> getDefinition() {
             return SEASON_PACKET;
-        }
-    }
-
-    public static final PacketDefinition<SubSeasonPacket, ByteBuf> SUBSEASON_PACKET = NETWORK_REGISTRY.registerClientbound("sync_subseason", SubSeasonPacket.CODEC);
-
-    public record SubSeasonPacket(int subSeason) implements NetworkPacket {
-        public static final NetworkCodec<SubSeasonPacket, ByteBuf> CODEC = CompositeCodecs.composite(
-                NetworkCodecs.INT, SubSeasonPacket::subSeason,
-                SubSeasonPacket::new);
-
-        @Override
-        public PacketDefinition<? extends NetworkPacket, ? extends ByteBuf> getDefinition() {
-            return SUBSEASON_PACKET;
-        }
-    }
-
-    public static final PacketDefinition<BiomePacket, ByteBuf> BIOME_PACKET = NETWORK_REGISTRY.registerClientbound("sync_biomes", BiomePacket.CODEC);
-
-    public record BiomePacket(String json) implements NetworkPacket {
-        public static final NetworkCodec<BiomePacket, ByteBuf> CODEC = CompositeCodecs.composite(
-                NetworkCodecs.STRING_UTF8, BiomePacket::json,
-                BiomePacket::new);
-
-        @Override
-        public PacketDefinition<? extends NetworkPacket, ? extends ByteBuf> getDefinition() {
-            return BIOME_PACKET;
-        }
-    }
-
-    public static final PacketDefinition<ReloadPacket, ByteBuf> RELOAD_RENDERER_PACKET = NETWORK_REGISTRY.registerClientbound("reload_renderer", ReloadPacket.CODEC);
-
-    public record ReloadPacket() implements NetworkPacket {
-        public static final NetworkCodec<ReloadPacket, ByteBuf> CODEC = NetworkCodecs.unit(new ReloadPacket());
-
-        @Override
-        public PacketDefinition<? extends NetworkPacket, ? extends ByteBuf> getDefinition() {
-            return RELOAD_RENDERER_PACKET;
         }
     }
 
